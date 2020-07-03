b629596616c4c99de5a7b629576664b51ffe6afe
Lily Wang <lily@minium.com.au>
2020-07-03 20:04:33 +1000

    update docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.0+4.g83a8fd2.dirty',
+    VERSION: '0.3.0+5.g5f321e7.dirty',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
